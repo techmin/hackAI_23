787910886caa301863a8b419484b98cda5dbbb80
bluestarburst <hargreaves.bryant@gmail.com>
2023-04-15 19:36:31 -0500

    a

--- src/app.js
+++ src/app.js
@@ -3,10 +3,39 @@ import { render } from 'react-dom'
 import { TextractClient, AnalyzeDocumentCommand } from "@aws-sdk/client-textract";
 
 // a client can be shared by different commands.
-const client = new TextractClient({ region: "REGION" });
+const client = new TextractClient({ 
+    region: 'us-east-1',
+    credentials: {
+        accessKeyId: 'AKIAYJLOY2F3I3NQ5UHX',
+        secretAccessKey: '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'
+    }
+ });
 
+
+ // get the data from the image and send it to the api
 const params = {
-    /** input parameters */
+    Document: {
+        // Bytes: new Buffer('...') || 'STRING_VALUE' /* Strings will be Base-64 encoded on your behalf */,
+        S3Object: {
+            Bucket: 'textract-console-us-east-1-4f4e2f2a-4b3a-4b2a-8b0a-2b0b0b0b0b0b',
+            Name: 'test.png',
+            // Version: 'STRING_VALUE'
+        }
+    },
+    FeatureTypes: [
+        'TABLES',
+        'FORMS'
+    ],
+    // HumanLoopConfig: {
+    //     HumanLoopName: 'STRING_VALUE',
+    //     FlowDefinitionArn: 'STRING_VALUE',
+    //     DataAttributes: {
+    //         ContentClassifiers: [
+    //             'FreeOfPersonallyIdentifiableInformation'|'FreeOfAdultContent',
+    //             /* more items */
+    //         ]
+    //     }
+
 };
 const command = new AnalyzeDocumentCommand(params);
 
@@ -17,13 +46,14 @@ function App () {
         // async/await.
         try {
             const data = await client.send(command);
+            console.log(data);
             // process data.
         } catch (error) {
             console.log(error);
             // error handling.
         } finally {
             console.log('finally');
-            console.log(data);
+            
             // finally.
         }
     }
